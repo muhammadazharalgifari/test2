44b5a50bd0e85a9f39738907fb4c842739ee78c6
muhammadazharalgifari <azhargifari22@gmail.com>
2024-06-04 00:06:05 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aspernatur eos enim omnis placeat aut. Incidunt obcaecati deleniti exercitationem. Fugiat dignissimos soluta consequuntur provident nobis sed velit inventore eum placeat nemo.</p>
+    <ul>
+        <li>list 1</li>
+        <li>list 2</li>
+        <li>list 3</li>
+    </ul>
 </body>
 </html>
